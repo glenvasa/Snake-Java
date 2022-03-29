80208bdf4d2df014b849b71310c43509c7a84ed3
Glen R. Vasa <glenvasa@gmail.com>
2022-03-29 08:44:14 -0400

    Update GamePanel.java

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GamePanel {
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+	
+	GamePanel(){
+		
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
